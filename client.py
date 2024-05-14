41f1142a359b87f3145c56e9e68067c88a2309ec
PedroCase <136331098+PedroCase@users.noreply.github.com>
2024-05-14 09:38:13 -0300

    Add files via upload

--- client.py
+++ client.py
@@ -1,1 +1,14 @@
 from socket import socket, AF_INET, SOCK_STREAM
+
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect(('127.0.0.1',12345))
+
+
+for _ in range(3):
+    data = input()
+
+    clientSocket.send(data.encode())
+
+    rep = clientSocket.recv(1024)
+
+    print(f"Resposta do servidor: {rep.decode()}")
--- servidor.py
+++ servidor.py
@@ -1,3 +1,31 @@
 from socket import socket, AF_INET, SOCK_STREAM
+from threading import Thread
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
+
+
+serverSocket.bind(('127.0.0.1',12345))
+serverSocket.listen()
+
+def handleClient(socketClient, ClientAdress):
+    for i in range(3):
+        req = socketClient.recv(1024)
+
+        print(f"{ClientAdress}>> A requisição {i+1} foi {req.decode()}")
+
+        rep = "Mensagem recebida!"
+
+        socketClient.send(rep.encode())
+    socketClient.close()
+    print(f"Conexão finalizada com Cliente de addr {ClientAdress}.")
+
+while True:
+    print("Servidor esperando requisições")
+
+    socketClient, ClientAdress = serverSocket.accept()
+
+    print(f"Cliente com addr {ClientAdress} estabeleceu a comunicação")
+
+    Thread(target=handleClient,args=[socketClient, ClientAdress]).start() 
+
+    
